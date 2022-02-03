39df636297fff801a5a53087174e34dfb9efd376
Aditya Gaurav <adityagaurav28@gmail.com>
2022-02-03 17:51:38 +0530

    Automated Match Fetch at the interval of 60 minutes

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,10 +1,4 @@
 from django.shortcuts import render
-from opencage.geocoder import OpenCageGeocode
-
-import requests
-import json
-from bs4 import BeautifulSoup
-import datetime
 
 from . import models
 
@@ -13,7 +7,6 @@ from . import tasks
 # Create your views here.
 
 def index(request):
-    tasks.fetchMatch()
     tasks.weatherAndScoreSummary()
     givenDates = models.Dates_Detail.objects.all()
     return render(request, 'mainapp/main.html', {
